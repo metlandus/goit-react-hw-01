cce41a3897d93ca01249508cd69e49b77a256cba
Mete <metlandus@hotmail.com.tr>
2024-11-09 20:56:28 +0300

    Task 1 is Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import ProfileCard from './components/ProfileCard'
+import userData from './data/userData.json'
 import AnimalList from './components/AnimalList'
 import DataTable from './components/DataTable'
 
@@ -7,12 +8,16 @@ function App() {
     return (
         <>
             <div className='app'>
-                <ProfileCard />
-                <AnimalList />
-                <DataTable />
+                <ProfileCard
+                    name={userData.username}
+                    tag={userData.tag}
+                    location={userData.location}
+                    image={userData.avatar}
+                    stats={userData.stats} />
             </div>
         </>
     );
 }
 
 export default App
+
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
-export const ProfileCard = () => {
+function ProfileCard({ name, tag, location, stats, image }) {
     return (
-        <div className='profile-card'>ProfileCard</div>
+        <div>
+            <div className='profile-card'>
+                <img src={image} alt={`${name}'s avatar`} />
+                <p>{name}</p>
+                <p>@{tag}</p>
+                <p>{location}</p>
+            </div>
+            <ul>
+                <li>
+                    <span>Followers: </span>
+                    <span>{stats.followers}</span>
+                </li>
+                <li>
+                    <span>Views: </span>
+                    <span>{stats.views}</span>
+                </li>
+                <li>
+                    <span>Likes: </span>
+                    <span>{stats.likes}</span>
+                </li>
+            </ul>
+        </div>
     )
 }
 
