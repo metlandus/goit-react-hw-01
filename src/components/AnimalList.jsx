51eea5aa9e403d938aede3b182d28f264d3b4ff1
Mete <metlandus@hotmail.com.tr>
2024-11-12 15:11:36 +0300

    friends list mostly done

--- src/components/AnimalList.jsx
+++ src/components/AnimalList.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function AnimalList({ friends }) {
     return (
-        <ul>
+        <ul className='flex flex-row items-center justify-center'>
             {friends.map((friend, id) => (
-                <li key={id}>
-                    <img src={friend.avatar} alt="avatar" />
-                    <p>{friend.name}</p>
-                    <p>{friend.isOnline ? 'online' : 'offline'}</p>
+                <li key={id} className='py-4 m-4 w-1/5 flex flex-col items-center justify-center border-4 border-black/80 rounded-xl'>
+                    <img width={100} height={100} src={friend.avatar} alt="avatar" />
+                    <p className='text-3xl font-medium'>{friend.name}</p>
+                    <p className={`${friend.isOnline ? "text-green-600" : "text-red-600"} font-medium font-xl`}>{friend.isOnline ? 'Online' : 'Offline'}</p>
                 </li>
             ))}
         </ul>
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -2,28 +2,30 @@ import React from 'react'
 
 function ProfileCard({ name, tag, location, stats, image }) {
     return (
-        <div className='profile-card flex flex-col items-center border-2 rounded-xl border-black w-1/2'>
-            <div className="flex flex-col justify-center items-center p-2">
-                <img className='rounded-full bg-black/85' width={250} height={250} src={image} alt={`${name}'s avatar`} />
-                <p className='text-3xl font-bold  p-2'>{name}</p>
-                <p className='text-xl font-semibold text-black/50'>@{tag}</p>
-                <p className='text-xl font-semibold text-black/50'>{location}</p>
-            </div>
-            <div className='w-full'>
-                <ul className='flex flex-row bg-black/20 border-t-2 border-black/40 justify-between'>
-                    <li className='p-4 flex flex-col items-center border-r-2 border-black/40 w-1/3'>
-                        <span className='text-lg font-medium'>Followers </span>
-                        <span className='text-lg font-extrabold'>{stats.followers}</span>
-                    </li>
-                    <li className='p-4 flex flex-col items-center border-r-2 border-black/40 w-1/3'>
-                        <span className='text-lg font-medium'>Views </span>
-                        <span className='text-lg font-extrabold'>{stats.views}</span>
-                    </li>
-                    <li className='p-4 flex flex-col items-center w-1/3'>
-                        <span className='text-lg font-medium'>Likes </span>
-                        <span className='text-lg font-extrabold'>{stats.likes}</span>
-                    </li>
-                </ul>
+        <div className='flex flex-col items-center justify-center'>
+            <div className='profile-card flex flex-col items-center border-2 rounded-xl border-black w-1/3'>
+                <div className="flex flex-col justify-center items-center p-2">
+                    <img className='rounded-full bg-black/85' width={250} height={250} src={image} alt={`${name}'s avatar`} />
+                    <p className='text-3xl font-bold  p-2'>{name}</p>
+                    <p className='text-xl font-semibold text-black/50'>@{tag}</p>
+                    <p className='text-xl font-semibold text-black/50'>{location}</p>
+                </div>
+                <div className='w-full'>
+                    <ul className='flex flex-row bg-black/20 border-t-2 border-black/40 justify-between'>
+                        <li className='p-4 flex flex-col items-center border-r-2 border-black/40 w-1/3'>
+                            <span className='text-lg font-medium'>Followers </span>
+                            <span className='text-lg font-extrabold'>{stats.followers}</span>
+                        </li>
+                        <li className='p-4 flex flex-col items-center border-r-2 border-black/40 w-1/3'>
+                            <span className='text-lg font-medium'>Views </span>
+                            <span className='text-lg font-extrabold'>{stats.views}</span>
+                        </li>
+                        <li className='p-4 flex flex-col items-center w-1/3'>
+                            <span className='text-lg font-medium'>Likes </span>
+                            <span className='text-lg font-extrabold'>{stats.likes}</span>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
